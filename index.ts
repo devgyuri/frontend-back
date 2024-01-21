a5a399b8a8824529bc56d98a328f5f7c4f59af8a
devgyuri <gksmf910@gmail.com>
2024-01-21 22:56:31 +0900

    connecting postgresql

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-const qqq: string = "Hello";
+import { DataSource } from "typeorm";
+import { Board } from "./Board.postgres";
 
-console.log(qqq);
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "127.0.0.1",
+  port: 5432,
+  username: "gyuri",
+  password: "memo2231",
+  database: "nodedb",
+  entities: [Board],
+  synchronize: true,
+  logging: true,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("DB접속에 성공했습니다.");
+  })
+  .catch((error) => {
+    console.log("DB접속에 실패했습니다.");
+    console.log(error);
+  });
